62336a38844aa6b5b048be23b513c8c6f58dd22e
Cyrus <cdbalsara@icloud.com>
2016-12-07 18:11:26 +0000

    listed all the diferent album types

--- public/app.js
+++ public/app.js
@@ -14,9 +14,36 @@ request.send();
 
 var requestComplete = function(){
 if(this.status !== 200) return;
-console.log("should appear if url correct")
+console.log("should appear if url correct");
 
-
+var jsonString = this.responseText;
+var albums = JSON.parse(jsonString);
+populateList(albums);
 }
+
+var populateList = function(albums){
+  var newAlbum = document.getElementById('albums');
+ console.log(albums);
+ for(album of albums.albums.items){
+  var li1 = document.createElement("li");
+  li1.innerText = album.name;
+  console.log(li1);
+newAlbum.appendChild(li1)
+}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.onload = app;
